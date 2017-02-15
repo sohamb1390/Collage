db42d1b1078b1e5cd3423499d4b5335c05b243c1
Soham <sohamb.1390@gmail.com>
2017-02-15 11:41:38 +0530

    Initial Project commit

---
+++ Collage/Collage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collage
+//
+//  Created by Soham Bhattacharjee on 15/02/17.
+//  Copyright © 2017 Soham Bhattacharjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
